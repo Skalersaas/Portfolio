7ee0d6e34a926748000d568378746682a1f18999
Skalersaas <efvhdg@gmail.com>
2025-01-28 15:44:12 +0400

    try 3

--- src/components/Certifications.vue
+++ src/components/Certifications.vue
@@ -2,27 +2,27 @@
 const data = [
     {
         href: 'https://www.hackerrank.com/certificates/649ed0c02e45',
-        src: '/src/assets/certifications/problem_solving.png'
+        src: '/certifications/problem_solving.png'
     },
     {
         href: 'https://www.hackerrank.com/certificates/c3a5a8c995f0',
-        src: '/src/assets/certifications/soft-engineer.png'
+        src: '/certifications/soft-engineer.png'
     },
     {
         href: 'https://www.hackerrank.com/certificates/6dea56357eb3',
-        src: '/src/assets/certifications/c_sharp.png'
+        src: '/certifications/c_sharp.png'
     },
     {
         href: 'https://www.hackerrank.com/certificates/7d991cde9c62',
-        src: '/src/assets/certifications/python.png'
+        src: '/certifications/python.png'
     },    
     {
         href: 'https://www.freecodecamp.org/certification/fcc103dcaa4-6b29-479c-b7d4-06761a1e6a76/foundational-c-sharp-with-microsoft',
-        src: '/src/assets/certifications/c_sharp2.png'
+        src: '/certifications/c_sharp2.png'
     },
     {
         href:'#',
-        src: '/src/assets/certifications/system-analyst.png'
+        src: '/certifications/system-analyst.png'
     },
     {
         href: '#',

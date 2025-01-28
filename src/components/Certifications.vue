f3a2e298ab5dd1165a7ee8417458ae79edc26bbf
Skalersaas <efvhdg@gmail.com>
2025-01-28 15:39:05 +0400

    try 2

--- src/components/Certifications.vue
+++ src/components/Certifications.vue
@@ -2,27 +2,27 @@
 const data = [
     {
         href: 'https://www.hackerrank.com/certificates/649ed0c02e45',
-        src: '/public/images/problem_solving.png'
+        src: '/src/assets/certifications/problem_solving.png'
     },
     {
         href: 'https://www.hackerrank.com/certificates/c3a5a8c995f0',
-        src: '/public/images/soft-engineer.png'
+        src: '/src/assets/certifications/soft-engineer.png'
     },
     {
         href: 'https://www.hackerrank.com/certificates/6dea56357eb3',
-        src: '/public/images/c_sharp.png'
+        src: '/src/assets/certifications/c_sharp.png'
     },
     {
         href: 'https://www.hackerrank.com/certificates/7d991cde9c62',
-        src: '/public/images/python.png'
+        src: '/src/assets/certifications/python.png'
     },    
     {
         href: 'https://www.freecodecamp.org/certification/fcc103dcaa4-6b29-479c-b7d4-06761a1e6a76/foundational-c-sharp-with-microsoft',
-        src: '/public/images/c_sharp2.png'
+        src: '/src/assets/certifications/c_sharp2.png'
     },
     {
         href:'#',
-        src: '/public/images/system-analyst.png'
+        src: '/src/assets/certifications/system-analyst.png'
     },
     {
         href: '#',
--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="ml-3 mt-4 flex items-end">
-        <img src="/public/images/profile.png" alt="profle" class="rounded-lg w-32 h-32">
+        <img src="/images/profile.png" alt="profile" class="rounded-lg w-32 h-32">
         <div class="m-2">
             <h1 class="text-lg font-bold">{{ $t('aboutme.name') }}</h1>
             <h2 class="text-sm">{{ $t('aboutme.profession') }}</h2>
